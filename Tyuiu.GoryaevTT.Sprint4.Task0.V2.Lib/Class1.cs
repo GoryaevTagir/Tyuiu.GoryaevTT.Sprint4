348e68f3fc2006f2be7e455e6916c4b833add446
GoryaevTagir <Lobstera134@gmail.com>
2025-10-18 01:37:14 +0000

    Add project files.

---
+++ Tyuiu.GoryaevTT.Sprint4.Task0.V2.Lib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.GoryaevTT.Sprint4.Task0.V2.Lib
+{
+    public class Class1 : ISprint4Task0V2
+    {
+        public int GetSumOddArrEl(int[] array)
+        {
+            int sum = 0;
+            foreach (int a in array)
+            {
+                if (a % 2 != 0)
+                {
+                    sum += a;
+                }
+            } 
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.GoryaevTT.Sprint4.Task0.V2.Test/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GoryaevTT.Sprint4.Task0.V2.Lib;
+namespace Tyuiu.GoryaevTT.Sprint4.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 ds = new Class1();
+            int[] ar = new int[] { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            int res = ds.GetSumOddArrEl(ar);
+            Assert.AreEqual(44, res);
+        }
+    }
+}
