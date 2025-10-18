e54f57c15b30659c0b90b1efb76937886682a13a
GoryaevTagir <Lobstera134@gmail.com>
2025-10-18 02:11:28 +0000

    Task1

---
+++ Tyuiu.GoryaevTT.Sprint4.Task1.V25.Lib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.GoryaevTT.Sprint4.Task1.V25.Lib
+{
+    public class Class1 : ISprint4Task1V25
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 0;
+            foreach (int a in array)
+            {
+                if (a % 2 == 0)
+                {
+                    sum += a;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.GoryaevTT.Sprint4.Task1.V25.Test/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GoryaevTT.Sprint4.Task1.V25.Lib;
+namespace Tyuiu.GoryaevTT.Sprint4.Task1.V25.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 ds = new Class1();
+            int[] ar = new int[] { 6, 3, 6, 4, 7, 4, 6, 5, 2, 3, 6, 3, 6, 2 };
+            int res = ds.Calculate(ar);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
